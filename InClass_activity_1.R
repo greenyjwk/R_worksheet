01ba8f95db4d7f22b795d59436c821d51aebae95
Ji <greenyjwk@gmail.com>
2022-09-18 19:33:16 -0400

    ADD: second activity files

--- InClass_activity_1.R
+++ InClass_activity_1.R
@@ -67,3 +67,76 @@ plot(x, y, xlab="this is x-axis", ylab="this is y-axis")
 pdf("FigurePlot.pdf")
 plot(x,y,col="green")
 dev.off()
+
+
+
+x = seq(1,10)
+x
+
+x = 1:11
+x
+
+x = seq(-pi, pi)
+x
+
+# outer function
+x=seq(-pi, pi, length=50)
+x
+y=x
+f=outer(x,y, function(x,y)cos(y)/(1+x^2))
+contour(x,y,f)
+contour(x, y, f, nlevel=45, add=T)
+
+image(x,y,f)
+persp(x,y,f, theta=30)
+persp(x,y,f, theta=30, phi=20)
+
+# indexing data
+A = matrix(1:16, 4,4,)
+A
+A[2,3]
+
+A[1,]
+
+A[c(1,3), c(2,4)]
+
+A[1:3, 2:4]
+
+A[1:2,]
+
+A[,1:2]
+
+# subtracting row 1 and row 3
+A[-c(1,3),]
+A
+
+A[-c(1,3), -c(1,3,4)]
+
+dim(A)
+
+
+# Loading the Auto.data file
+
+Auto = read.table("Auto.data", header=T, na.strings="?")
+dim(Auto)
+fix(Auto)
+
+Auto[1:4,]
+
+Auto=na.omit(Auto)
+dim(Auto)
+
+
+Auto(header)
+plot(Auto$cylinders, Auto$mpg)
+
+attach(Auto)
+plot(Auto$cylinders, Auto$mpg)
+
+cylinders = as.factor(cylinders)
+plot(cylinders, mpg)
+plot(cylinders, mpg, col ="red", varwidth = T)
+plot(cylinders, mpg, col ="red", varwidth = T, horizontal= T)
+hist(mpg)
+pairs(Auto[,1:8])
+
