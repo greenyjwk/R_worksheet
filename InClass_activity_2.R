b3e47289a05f8a3bafea568dd3b1e9c35b26e944
Ji <greenyjwk@gmail.com>
2022-09-19 14:13:22 -0400

    Lineare Regrssion Evalutation

--- InClass_activity_2.R
+++ InClass_activity_2.R
@@ -1,2 +1,21 @@
 library(MASS)
 library(ISLR)
+names(Boston)
+#lm(y~x, data)
+lm.fit = lm(medv~lstat, data = Boston)
+lm.fit
+attach(Boston)
+lm.fit = lm(medv~lstat, data=Boston)
+
+print("---------- Lineare Regression Model Summary ----------")
+summary(lm.fit)
+
+names(lm.fit)
+
+confint(lm.fit)
+
+predict(lm.fit, data.frame(lstat = (c(5,10,15))),interval = "confidence")
+# The model provides 95%, 90%, 85% assurance of prediction
+
+predict(lm.fit, data.frame(lstat = (c(5,10,15))),interval = "prediction")
+# It has wider range since it has epsilon
